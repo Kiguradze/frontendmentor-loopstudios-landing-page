e08ee02b63da9dc30af8408e04c0d2dfb521863c
Beso Kiguradze <bkiguradze@gmail.com>
2023-03-16 19:53:06 +0400

    change css file

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,23 +4,23 @@ import Nav from "./nav/Nav";
 import Menu from "./menu/Menu";
 
 const Header = () => {
-  const [active, setActive] = useState(false);
+   const [active, setActive] = useState(false);
 
-  const togleClick = () => {
-    setActive(!active);
-  };
+   const togleClick = () => {
+      setActive(!active);
+   };
 
-  return (
-    <div className="header">
-      <Nav onclick={togleClick} />
-      <Menu active={active} onclick={togleClick} />
-      <div className="container">
-        <div className="desc">
-          <p className="desc-text">Immersive experiences that deliver</p>
-        </div>
+   return (
+      <div className="header">
+         <Nav onclick={togleClick} />
+         <Menu active={active} onclick={togleClick} />
+         <div className="container">
+            <div className="desc">
+               <p className="desc-text">Immersive experiences that deliver</p>
+            </div>
+         </div>
       </div>
-    </div>
-  );
+   );
 };
 
 export default Header;
